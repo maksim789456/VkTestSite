ea9ebf0a7148cbd7fc091397a0fc99a40ab4c848
maksim789456 <git@capybaraesttravy.ru>
2025-06-12 20:22:50 +0400

    feat: impl helper class for create descriptor sets and pipeline layout

---
+++ src/DescriptorSet.cpp
@@ -0,0 +1,115 @@
+#include "DescriptorSet.h"
+
+DescriptorSet::DescriptorSet() = default;
+
+DescriptorSet::DescriptorSet(
+  const vk::Device &device,
+  const vk::DescriptorPool &descriptorPool,
+  const uint32_t descriptorSetCount,
+  const std::vector<DescriptorLayout> &layouts,
+  const std::vector<vk::PushConstantRange> &push_consts
+) {
+  m_descriptorSetCount = descriptorSetCount;
+  setup_layout(device, layouts, push_consts);
+  create(device, descriptorPool);
+}
+
+/**
+ * Create descriptor set layout and pipeline layout by provided DS layouts
+ * @param device refence to logical device
+ * @param layouts DS layouts
+ * @param push_consts Push constants info
+ */
+void DescriptorSet::setup_layout(
+  const vk::Device &device,
+  const std::vector<DescriptorLayout> &layouts,
+  const std::vector<vk::PushConstantRange> &push_consts
+) {
+  m_descriptorLayouts = layouts;
+
+  auto layoutBindingsFlags = std::vector<vk::DescriptorBindingFlags>();
+  auto layoutBindings = std::vector<vk::DescriptorSetLayoutBinding>();
+  for (const auto &layout: m_descriptorLayouts) {
+    layoutBindingsFlags.emplace_back(layout.bindingFlags);
+    layoutBindings.emplace_back(
+      layout.shaderBinding, layout.type, layout.count, layout.stage
+    );
+  }
+
+  const auto dslFlags =
+      std::ranges::find(layoutBindingsFlags, vk::DescriptorBindingFlagBits::eUpdateAfterBind)
+      != layoutBindingsFlags.end()
+        ? vk::DescriptorSetLayoutCreateFlagBits::eUpdateAfterBindPool
+        : vk::DescriptorSetLayoutCreateFlags{};
+
+  auto dslInfo = vk::DescriptorSetLayoutCreateInfo(dslFlags, layoutBindings);
+  const auto flagsInfo = vk::DescriptorSetLayoutBindingFlagsCreateInfo(layoutBindingsFlags);
+  dslInfo.pNext = &flagsInfo;
+  m_descriptorSetLayout = device.createDescriptorSetLayout(dslInfo);
+
+  const auto plInfo = vk::PipelineLayoutCreateInfo({}, m_descriptorSetLayout, push_consts);
+  m_pipelineLayout = device.createPipelineLayout(plInfo);
+}
+
+/**
+ * Allocate descriptor sets - count as swapchain images count
+ * by descriptor set layout created on setup_layout
+ * @param device refence to logical device
+ * @param descriptorPool refence to descriptor pool, pool must contain an enough items
+ */
+void DescriptorSet::create(const vk::Device &device, const vk::DescriptorPool &descriptorPool) {
+  const auto setLayouts = std::vector(m_descriptorSetCount, m_descriptorSetLayout);
+  const auto info = vk::DescriptorSetAllocateInfo(descriptorPool, setLayouts);
+  m_descriptorSets = device.allocateDescriptorSets(info);
+
+  auto descriptorWrites = std::vector<vk::WriteDescriptorSet>();
+  for (uint32_t i = 0; i < m_descriptorSetCount; i++) {
+    descriptorWrites.clear();
+    const auto &descriptorSet = m_descriptorSets[i];
+    for (const auto &layout: m_descriptorLayouts) {
+      auto writeInfo = vk::WriteDescriptorSet(descriptorSet, layout.shaderBinding, {}, layout.count, layout.type);
+      if (layout.type == vk::DescriptorType::eUniformBuffer) {
+        try {
+          auto bufferInfo = layout.bufferInfos.at(i);
+          writeInfo.setBufferInfo(bufferInfo);
+          descriptorWrites.push_back(writeInfo);
+        } catch (std::out_of_range &_) {
+        }
+      } else if (layout.type == vk::DescriptorType::eCombinedImageSampler) {
+        try {
+          auto imageInfo = layout.imageInfos.at(i);
+          writeInfo.setImageInfo(imageInfo);
+          descriptorWrites.push_back(writeInfo);
+        } catch (std::out_of_range &_) {
+        }
+      }
+    }
+
+    for (const auto &descriptorWrite: descriptorWrites) {
+      device.updateDescriptorSets(descriptorWrite, {});
+    }
+  }
+}
+
+void DescriptorSet::bind(
+  const vk::CommandBuffer &commandBuffer,
+  const uint32_t currentFrameIdx,
+  const std::vector<uint32_t> &dynamicOffsets
+) const {
+  commandBuffer.bindDescriptorSets(
+    vk::PipelineBindPoint::eGraphics,
+    m_pipelineLayout,
+    0,
+    m_descriptorSets[currentFrameIdx],
+    dynamicOffsets
+  );
+}
+
+const vk::PipelineLayout &DescriptorSet::getPipelineLayout() const {
+  return m_pipelineLayout;
+}
+
+void DescriptorSet::destroy(const vk::Device &device) const {
+  device.destroyPipelineLayout(m_pipelineLayout);
+  device.destroyDescriptorSetLayout(m_descriptorSetLayout);
+}
---
+++ src/DescriptorSet.h
@@ -0,0 +1,59 @@
+#ifndef DESCRIPTORSET_H
+#define DESCRIPTORSET_H
+
+#include <vulkan/vulkan.hpp>
+
+struct DescriptorLayout {
+  vk::DescriptorType type;
+  vk::ShaderStageFlags stage;
+  vk::DescriptorBindingFlags bindingFlags;
+  uint32_t shaderBinding;
+  uint32_t count;
+
+  std::vector<vk::DescriptorImageInfo> imageInfos;
+  std::vector<vk::DescriptorBufferInfo> bufferInfos;
+};
+
+class DescriptorSet {
+public:
+  DescriptorSet();
+
+  DescriptorSet(
+    const vk::Device &device,
+    const vk::DescriptorPool &descriptorPool,
+    uint32_t descriptorSetCount,
+    const std::vector<DescriptorLayout> &layouts,
+    const std::vector<vk::PushConstantRange> &push_consts
+  );
+
+  void bind(
+    const vk::CommandBuffer &commandBuffer,
+    uint32_t currentFrameIdx,
+    const std::vector<uint32_t> &dynamicOffsets
+  ) const;
+
+  [[nodiscard]] const vk::PipelineLayout &getPipelineLayout() const;
+  void destroy(const vk::Device &device) const;
+
+private:
+  uint32_t m_descriptorSetCount = 0;
+  std::vector<DescriptorLayout> m_descriptorLayouts;
+  //std::vector<vk::DescriptorSetLayoutBinding> m_layoutsBindings;
+
+  vk::PipelineLayout m_pipelineLayout;
+  vk::DescriptorSetLayout m_descriptorSetLayout;
+  std::vector<vk::DescriptorSet> m_descriptorSets;
+
+  void setup_layout(
+    const vk::Device &device,
+    const std::vector<DescriptorLayout> &layouts,
+    const std::vector<vk::PushConstantRange> &push_consts
+  );
+
+  void create(
+    const vk::Device &device,
+    const vk::DescriptorPool &descriptorPool
+  );
+};
+
+#endif //DESCRIPTORSET_H
