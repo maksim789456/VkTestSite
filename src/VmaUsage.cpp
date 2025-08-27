fb6bdc0ddfcdc746af6883e29eec938cd73cd885
maksim789456 <git@capybaraesttravy.ru>
2025-08-27 18:22:04 +0400

    fix: don't print vma debug log more

--- src/VmaUsage.cpp
+++ src/VmaUsage.cpp
@@ -1,7 +1,3 @@
 #define VMA_IMPLEMENTATION
 #define VMA_VULKAN_VERSION 1003000
-#define VMA_DEBUG_LOG(format, ...) do { \
-printf(format, __VA_ARGS__); \
-printf("\n"); \
-} while(false)
 #include "vulkan-memory-allocator-hpp/vk_mem_alloc.hpp"
