be9f144ffb3850474961fd2f41941c07c431a2f0
maksim789456 <git@capybaraesttravy.ru>
2025-09-02 22:29:52 +0400

    feat: refactoring of shader module, use spv reflect

--- src/ShaderModule.cpp
+++ src/ShaderModule.cpp
@@ -1,37 +1,53 @@
 #include "ShaderModule.h"
 
-#include <iostream>
-
-void ShaderModule::load(const std::string &path) {
+void ShaderModule::load(
+  const vk::Device &device,
+  const std::string &path
+) {
   ZoneScoped;
   auto file = std::ifstream(path, std::ios::binary | std::ios::ate);
-  if (!file.is_open()) {
+  if (file.fail() || !file.is_open()) {
     std::cerr << "Failed to open shader source file" << std::endl;
     abort();
   }
-  auto fileSize = static_cast<size_t>(file.tellg());
-  std::vector<char> spv(fileSize);
-  file.seekg(0);
-  file.read(spv.data(), fileSize);
 
-  this->spv = spv;
+  const auto fileSize = static_cast<uint32_t>(file.tellg());
+  const auto bufferSize = fileSize / sizeof(uint32_t);
+  std::vector<uint32_t> spv(bufferSize);
+
+  file.seekg(0);
+  auto *fileData = reinterpret_cast<char *>(spv.data());
+  file.read(fileData, fileSize);
   file.close();
+
+  this->m_spv = spv;
+  const auto info = vk::ShaderModuleCreateInfo({}, spv.size() * sizeof(uint32_t), spv.data());
+  m_module = device.createShaderModuleUnique(info);
 }
 
-void ShaderModule::compile(
+void ShaderModule::reflect(
   const vk::Device &device
 ) {
   ZoneScoped;
-  //const auto spvModule = spv_reflect::ShaderModule(spv);
-  //spvModule.EnumerateDescriptorSets(nullptr, nullptr);
+  m_spvReflectModule = std::make_unique<spv_reflect::ShaderModule>(m_spv);
+  if (m_spvReflectModule->GetResult() != SPV_REFLECT_RESULT_SUCCESS) {
+    std::cerr << "Failed to reflect shader module" << std::endl;
+    abort();
+  }
 
-  const auto info = vk::ShaderModuleCreateInfo({}, spv.size(), reinterpret_cast<const uint32_t*>(spv.data()));
-  module = device.createShaderModule(info);
-  vertexPipelineInfo = vk::PipelineShaderStageCreateInfo({}, vk::ShaderStageFlagBits::eVertex, module, "vertexMain");
-  fragmentPipelineInfo = vk::PipelineShaderStageCreateInfo({}, vk::ShaderStageFlagBits::eFragment, module,
-                                                           "fragmentMain");
-}
+  for (int i = 0; i < m_spvReflectModule->GetEntryPointCount(); ++i) {
+    const auto ep = spvReflectGetEntryPoint(
+      &m_spvReflectModule->GetShaderModule(),
+      m_spvReflectModule->GetEntryPointName(i)
+    );
 
-void ShaderModule::destroy(const vk::Device &device) const {
-  device.destroyShaderModule(module);
+    if (ep->shader_stage & SPV_REFLECT_SHADER_STAGE_VERTEX_BIT) {
+      vertexPipelineInfo = vk::PipelineShaderStageCreateInfo(
+        {}, vk::ShaderStageFlagBits::eVertex, m_module.get(), ep->name);
+    }
+    if (ep->shader_stage & SPV_REFLECT_SHADER_STAGE_FRAGMENT_BIT) {
+      fragmentPipelineInfo = vk::PipelineShaderStageCreateInfo(
+        {}, vk::ShaderStageFlagBits::eFragment, m_module.get(), ep->name);
+    }
+  }
 }
