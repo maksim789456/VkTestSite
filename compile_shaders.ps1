21bdcaf833a6731d7ede7787aaa450a1a2221196
maksim789456 <git@capybaraesttravy.ru>
2025-09-04 06:00:01 +0400

    shaders: enable generate debug info

--- compile_shaders.ps1
+++ compile_shaders.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem -Path .\res\shaders\ -File -Recurse -exclude *.spv | ForEach-Object {
     $sourcePath = $_.fullname
     $targetPath = "$($_.fullname).spv"
-    slangc.exe -profile glsl_460 -capability spvShaderNonUniformEXT -target spirv -emit-spirv-directly -force-glsl-scalar-layout -fvk-use-entrypoint-name -entry vertexMain -entry fragmentMain -o $targetPath $sourcePath
+    slangc.exe -profile glsl_460 -capability spvShaderNonUniformEXT -target spirv -emit-spirv-directly -force-glsl-scalar-layout -fvk-use-entrypoint-name -entry vertexMain -entry fragmentMain -g -o $targetPath $sourcePath
 }
