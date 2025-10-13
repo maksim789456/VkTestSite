5bbe9418a02e466f0684129ca666bb5576609230
maksim789456 <git@capybaraesttravy.ru>
2025-10-13 07:11:18 +0400

    feat: compile compute shaders

--- compile_shaders.ps1
+++ compile_shaders.ps1
@@ -3,3 +3,10 @@ Get-ChildItem -Path .\res\shaders\ -File -Recurse -include *.ep.slang -exclude *
     $targetPath = "$($_.fullname).spv"
     slangc.exe -profile glsl_460 -capability spvShaderNonUniformEXT -target spirv -emit-spirv-directly -force-glsl-scalar-layout -fvk-use-entrypoint-name -entry vertexMain -entry fragmentMain -g -DDEBUG=1 -o $targetPath $sourcePath
 }
+
+# Compute Shaders
+Get-ChildItem -Path .\res\shaders\ -File -Recurse -include *.cmp.slang -exclude *.spv | ForEach-Object {
+    $sourcePath = $_.fullname
+    $targetPath = "$($_.fullname).spv"
+    slangc.exe -profile glsl_460 -target spirv -emit-spirv-directly -force-glsl-scalar-layout -fvk-use-entrypoint-name -entry cmpMain -g -DDEBUG=1 -o $targetPath $sourcePath
+}
